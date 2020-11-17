8637e85ac096370d5c6a3682de1b8551e5986e2c
rdojeda <ojeda.robertodaniel@gmail.com>
2020-11-16 22:28:35 -0300

    Subida de Componentes y Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-   <h1>MERNTasks</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
+
   );
 }
 
---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevaCuenta = () => {
+    return (  
+
+        <h1>Desde NuevaCuenta</h1>
+
+    );
+}
+ 
+export default NuevaCuenta;
